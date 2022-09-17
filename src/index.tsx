d01917770deed93716a3e3759efa3dc85955ecb2
DaNaRim <rangar.danarim@gmail.com>
2022-09-18 00:14:40 +0300

    FIX some styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import {createRoot} from "react-dom/client"
 import {Provider} from "react-redux"
 import App from "./app/App"
 import {store} from "./app/store"
-import "./index.css"
 import reportWebVitals from "./reportWebVitals"
 
 const container = document.getElementById("root")!
